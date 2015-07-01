ea1642e4cb4d196f3bfc70516973aeeef76cc5cd
rodica-andronache <rodik_andronache@yahoo.com>
2015-07-01 16:34:25 +0300

    Fixed functions prefixes, redesigned latest news and about us section

--- functions.php
+++ functions.php
@@ -1,40 +1,59 @@
 <?php
 
-// add any new or customised functions here
-
-add_action( 'wp_enqueue_scripts', 'responsive_boat_enqueue_styles' );
-function responsive_boat_enqueue_styles() {
+add_action( 'wp_enqueue_scripts', 'responsiveboat_enqueue_styles' );
+function responsiveboat_enqueue_styles() {
 	
-	wp_enqueue_style( 'responsive_boat_font', '//fonts.googleapis.com/css?family=Titillium+Web:400,300,300italic,200italic,200,400italic,600,600italic,700,700italic,900');
+	wp_enqueue_style( 'responsiveboat-font', '//fonts.googleapis.com/css?family=Titillium+Web:400,300,300italic,200italic,200,400italic,600,600italic,700,700italic,900');
 	
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array('zerif_bootstrap_style') );
-
-	// Loads our main stylesheet.
-	 wp_enqueue_style( 'zerif-child-style', get_stylesheet_uri(), array('zerif_style'), 'v1' );
+	wp_enqueue_style( 'responsiveboat-style', get_template_directory_uri() . '/style.css', array('zerif_bootstrap_style') );
 
 }
 
-function responsive_boat_custom_script_fix()
-{
+function responsiveboat_custom_script_fix() {
 	if ( !wp_is_mobile() ){
 
-		wp_enqueue_script('zerif_script_child', get_stylesheet_directory_uri() .'/js/zerif.js', array('zerif_scrollReveal_script'), '201202067', true); 
+		wp_enqueue_script('responsiveboat-script', get_stylesheet_directory_uri() .'/js/zerif.js', array('zerif_scrollReveal_script'), '201202067', true); 
 
 	}else{
 
-		wp_enqueue_script('zerif_script_child', get_stylesheet_directory_uri() .'/js/zerif.js', array(), '201202067', true); 
+		wp_enqueue_script('responsiveboat-script', get_stylesheet_directory_uri() .'/js/zerif.js', array(), '201202067', true); 
+		
 		/*  reduce height of the google maps on mobile */
-		wp_enqueue_style( 'zerif-style-mobile', get_template_directory_uri() . '/css/zerif_mobile.css' );
+		wp_enqueue_style( 'responsiveboat-script-mobile', get_template_directory_uri() . '/css/zerif_mobile.css' );
 
 	}
-	wp_enqueue_script('zerif_nicescroll',get_stylesheet_directory_uri().'/js/jquery.nicescroll.js',array('jquery'),'12121',true);
-    wp_enqueue_script('zerif_nicescroll-script',get_stylesheet_directory_uri().'/js/zerif-nicescroll.js',array('jquery','zerif_nicescroll'),'12121',true);	
+	
+	wp_enqueue_script('responsiveboat-nicescroll',get_stylesheet_directory_uri().'/js/jquery.nicescroll.js',array('jquery'),'12121',true);
+    wp_enqueue_script('responsiveboat-nicescroll-script',get_stylesheet_directory_uri().'/js/zerif-nicescroll.js',array('jquery','responsiveboat-nicescroll'),'12121',true);	
 }
 
-add_action( 'wp_enqueue_scripts', 'responsive_boat_custom_script_fix' );
+add_action( 'wp_enqueue_scripts', 'responsiveboat_custom_script_fix' );
 
-function responsive_boat_remove_style_child(){
+function responsiveboat_remove_style_child(){
 	remove_action('wp_print_scripts','zerif_php_style');	
 }
 
-add_action( 'wp_enqueue_scripts', 'responsive_boat_remove_style_child', 100 );
+add_action( 'wp_enqueue_scripts', 'responsiveboat_remove_style_child', 100 );
+
+/* get first image from a post content or get a default image */
+function zerif_get_first_image_from_post() {
+
+	global $post, $posts;
+	
+	$zerif_first_img = '';
+	ob_start();
+	ob_end_clean();
+  
+	$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $zerif_matches);
+	
+	if( !empty($zerif_matches[1][0]) ):
+		$zerif_first_img = '<img src="'.$zerif_matches[1][0].'">';
+	endif;	
+
+	if(empty($zerif_first_img)):
+		$zerif_first_img = '<img src="'.get_template_directory_uri().'/images/blank-latestposts.png'.'">';
+	endif;
+	
+	return $zerif_first_img;
+	
+} 
