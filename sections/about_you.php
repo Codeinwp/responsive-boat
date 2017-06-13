a0f0ae0c234c3d1de34dea61c421632e6b84726c
Rodica-Elena Andronache <rodica.andronache@vertistudio.com>
2017-06-13 17:22:32 +0300

    #44 added back clients area

--- sections/about_you.php
+++ sections/about_you.php
@@ -72,6 +72,31 @@
 
         </div> <!-- / END 3 COLUMNS OF ABOUT US-->
 
+        <!-- CLIENTS -->
+        <?php
+        if(is_active_sidebar( 'sidebar-aboutus' )):
+
+            $zerif_aboutus_clients_title_text = get_theme_mod( 'zerif_aboutus_clients_title_text' );
+
+            echo '<div class="our-clients">';
+
+            if( !empty($zerif_aboutus_clients_title_text) ) {
+
+                echo '<h5><span class="section-footer-title">'.wp_kses_post( $zerif_aboutus_clients_title_text ).'</span></h5>';
+            } elseif ( is_customize_preview() ) {
+                echo '<h5><span class="section-footer-title"></span></h5>';
+            }
+
+            echo '</div>';
+
+            echo '<div class="client-list">';
+            echo '<div data-scrollreveal="enter right move 60px after 0.00s over 2.5s">';
+            dynamic_sidebar( 'sidebar-aboutus' );
+            echo '</div>';
+            echo '</div> ';
+        endif;
+        ?>
+
     </div> <!-- / END CONTAINER -->
 
 </section> <!-- END ABOUT US SECTION -->
